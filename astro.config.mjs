1b7e9ef8fa09c529e72836d4db7759b22e425e7a
Daniel Gómez-Sánchez <daniel.gomez.sanchez@hotmail.es>
2026-02-16 20:26:32 +0100

    add tailwind-typography, rephype-external-links and fix versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,24 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
+import rehypeExternalLinks from "rehype-external-links";
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	markdown: {
+		rehypePlugins: [
+			[
+				rehypeExternalLinks,
+				{
+					target: "_blank",
+					rel: [],
+					content: { type: "text", value: "🔗" },
+				},
+			],
+		],
+	},
 });
